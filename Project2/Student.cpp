144f0413c302d2a9b9d406e7f87534a9812f4c21
erodriguez13 <136417187+erodriguez13@users.noreply.github.com>
2023-12-20 11:31:47 -0600

    Add files via upload

---
+++ Project2/Student.cpp
@@ -0,0 +1,8 @@
+//
+//  Student.cpp
+//  RodriguezEProj2
+//
+//  Created by Eduardo Rodriguez on 2/5/23.
+//
+
+#include "Student.hpp"
---
+++ Project2/Student.hpp
@@ -0,0 +1,13 @@
+//
+//  Student.hpp
+//  RodriguezEProj2
+//
+//  Created by Eduardo Rodriguez on 2/5/23.
+//
+
+#ifndef Student_hpp
+#define Student_hpp
+
+#include <stdio.h>
+
+#endif /* Student_hpp */
